cc9f4517d184b58e7e4b9860645f0a96ad672e37
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-04-10 16:32:11 +0300

    insert dummy address ok

--- sql/DB_INSERT.sql
+++ sql/DB_INSERT.sql
@@ -1,1 +1,7 @@
 -- INSERT DUMMY DATA
+INSERT INTO
+    Addresses (Street, Number, City, Postal_Code)
+VALUES
+    ('Main street', '1', 'Αθήνα', '1010'),
+    ('Katexaki', '2', 'Αθήνα', '1010'),
+    ('Syggrou', '13', 'Αθήνα', '1010');

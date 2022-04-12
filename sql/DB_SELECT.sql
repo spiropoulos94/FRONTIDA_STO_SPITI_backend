4954e5ac69a271c0bf3fd85786a07b9bbb50093a
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-04-13 00:17:16 +0300

    find which users can edit which report authors

--- sql/DB_SELECT.sql
+++ sql/DB_SELECT.sql
@@ -8,3 +8,16 @@ from
     LEFT JOIN Roles loggedUsers ON rp.LoggedUserRole = loggedUsers.Role_id
     LEFT JOIN Permissions perms ON rp.Permission_id = perms.Permission_id
     LEFT JOIN Roles report_authors ON rp.Report_author_id = report_authors.Role_id;
+
+-- check doctors (2) permissions 
+select
+    loggedUsers.Title,
+    Permissions.Name,
+    reportAuthors.Title
+from
+    Reports_permissions reports_perms
+    left join Roles loggedUsers on reports_perms.LoggedUserRole = loggedUsers.Role_id
+    left join Permissions on reports_perms.Permission_id = Permissions.Permission_id
+    left join Roles reportAuthors on reports_perms.Report_author_id = reportAuthors.Role_id
+where
+    Reports_perms.LoggedUserRole = 2

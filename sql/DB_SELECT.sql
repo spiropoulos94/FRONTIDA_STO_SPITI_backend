d314477b74e2df92df2bb42c12168f36e9e0153b
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-04-12 23:31:50 +0300

    insert reports_permissions in Reports_permissions_table

--- sql/DB_SELECT.sql
+++ sql/DB_SELECT.sql
@@ -3,5 +3,5 @@ SELECT
 from
     Reports_permissions
     LEFT JOIN Roles ON Reports_permissions.Permission_id = Roles.Role_id
-    LEFT JOIN Permissions ON Reports_permissions.Permission_id = Permissions.Permission_id,
+    LEFT JOIN Permissions ON Reports_permissions.Permission_id = Permissions.Permission_id
     LEFT JOIN Roles ON Reports_permissions.Report_author_id = Roles.Role_id;

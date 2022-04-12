19bafe4b0267a3bd49c7821e0595fe0a7222ddef
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-04-12 23:59:18 +0300

    find which users can edit which report authors

--- sql/DB_SELECT.sql
+++ sql/DB_SELECT.sql
@@ -1,10 +1,10 @@
 -- check which roles can read or write which author
 SELECT
-    roles1.Title,
+    loggedUsers.Title,
     perms.Name,
-    roles2.Title
+    report_authors.Title
 from
     Reports_permissions rp
-    LEFT JOIN Roles roles1 ON rp.LoggedUserRole = roles1.Role_id
+    LEFT JOIN Roles loggedUsers ON rp.LoggedUserRole = loggedUsers.Role_id
     LEFT JOIN Permissions perms ON rp.Permission_id = perms.Permission_id
-    LEFT JOIN Roles roles2 ON rp.Report_author_id = roles2.Role_id;
+    LEFT JOIN Roles report_authors ON rp.Report_author_id = report_authors.Role_id;
